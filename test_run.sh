70482eaa9691a4b5ab0c7946b71c94a1917230dd
Vladimir Ignatev <ya.na.pochte@gmail.com>
2023-06-02 15:07:14 +0200

    clean up

--- test_run.sh
+++ test_run.sh
@@ -2,11 +2,11 @@
 
 cargo build -r
 
-ROCKET_LOG_LEVEL="critical" ROCKET_ADDRESS='127.0.0.1' ROCKET_WORKERS=64 ROCKET_PORT=8001 ROCKET_DATABASES='{redis={url="unix:///var/run/redis/redis.sock", min_connections=1024, max_connections=4096, connect_timeout=5, idle_timeout=120}}' target/release/eventsink &
+ROCKET_LOG_LEVEL="critical" ROCKET_ADDRESS='127.0.0.1' ROCKET_WORKERS=16 ROCKET_PORT=8001 ROCKET_DATABASES='{redis={url="redis://localhost:6379", min_connections=1024, max_connections=4096, connect_timeout=5, idle_timeout=120}}' target/release/eventsink &
 
 
 wrk='wrk.method = "POST"
-     wrk.headers["X-Test"] = "benchmark are fun! yeah"'
+     wrk.headers["X-Test"] = "benchmarks are funny! yeah"'
 echo $wrk > /tmp/wrk.post.lua
 
 wrk -s /tmp/wrk.post.lua -c800 -d30s -t16 http://localhost:8001/sink/teststream/ &
